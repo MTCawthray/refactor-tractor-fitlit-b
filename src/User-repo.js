7679ddbf8d5c998d539c923821b37b1286ffb1d4
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-27 19:59:36 -0600

    Create calcAvgAllUsers()

--- src/User-repo.js
+++ src/User-repo.js
@@ -2,43 +2,65 @@ class UserRepo {
   constructor(users) {
     this.users = users;
   }
+
   getDataFromID(id) { 
     return this.users.find((user) => id === user.id);
   }
+
   getDataFromUserID(id, dataSet) {
     return dataSet.filter((userData) => id === userData.userID);
   }
-  calculateAverageStepGoal() {
-    var totalStepGoal = this.users.reduce((sumSoFar, data) => {
-      return sumSoFar = sumSoFar + data.dailyStepGoal;
+
+
+
+// helper
+  calcAvgAllUsers(key) {
+    var result = this.users.reduce((acc, dataSet) => {
+      return acc += dataSet[key];
     }, 0);
-    return totalStepGoal / this.users.length;
+    return result / this.users.length;
   }
+
+// method using helper
+  calculateAverageStepGoal() {
+    this.calcAvgAllUsers('dailyStepGoal');
+  }
+
+
+
+  
+
   makeSortedUserArray(id, dataSet) {
     let selectedID = this.getDataFromUserID(id, dataSet)
     let sortedByDate = selectedID.sort((a, b) => new Date(b.date) - new Date(a.date));
     return sortedByDate;
   }
+
   getToday(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet)[0].date;
   }
+
   getFirstWeek(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet).slice(0, 7);
   }
+
   getWeekFromDate(date, id, dataSet) {
     let dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date)));
     return this.makeSortedUserArray(id, dataSet).slice(dateIndex, dateIndex + 7);
   }
+
   chooseWeekDataForAllUsers(dataSet, date) {
     return dataSet.filter(function(dataItem) {
       return (new Date(date)).setDate((new Date(date)).getDate() - 7) <= new Date(dataItem.date) && new Date(dataItem.date) <= new Date(date)
     })
   }
+
   chooseDayDataForAllUsers(dataSet, date) {
     return dataSet.filter(function(dataItem) {
       return dataItem.date === date
     });
   }
+
   isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod) {
     return listFromMethod.reduce(function(objectSoFar, dataItem) {
       if (!objectSoFar[dataItem.userID]) {
@@ -49,6 +71,7 @@ class UserRepo {
       return objectSoFar;
     }, {});
   }
+
   rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     return Object.keys(sortedObjectKeys).sort(function(b, a) {
@@ -61,6 +84,7 @@ class UserRepo {
       }, 0) / sortedObjectKeys[b].length)
     });
   }
+
   combineRankedUserIDsAndAveragedData(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     let rankedUsersAndAverages = this.rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod)
@@ -75,6 +99,7 @@ class UserRepo {
       return rankedUser;
     });
   }
+
 }
 
 export default UserRepo;
