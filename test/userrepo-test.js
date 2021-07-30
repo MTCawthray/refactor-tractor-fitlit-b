04af346658e7d2f8464bcb383917195f461cf92e
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-29 19:37:45 -0600

    Refactor get data from id test to check for other user

--- test/userrepo-test.js
+++ test/userrepo-test.js
@@ -1,14 +1,10 @@
 import { expect } from 'chai';
-
 import UserRepo from '../src/User-repo';
 import User from '../src/User';
 
 
 describe('User Repo', function() {
-  let user1;
-  let user2;
-  let user;
-  let userRepo;
+  let user1, user2, users, userRepo;
 
   beforeEach(function() {
     user1 = new User({
@@ -34,40 +30,23 @@ describe('User Repo', function() {
   });
 
   it('should be a function', function() {
-    const userRepo = new UserRepo();
-
     expect(UserRepo).to.be.a('function');
   });
 
   it('takes an array of user data', function() {
-
     expect(userRepo.users).to.include(user2);
   });
 
   it('should have a parameter to take in user data', function() {
-    const user1 = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    const users = [user1];
+    const users = [user1, user2];
     const userRepo = new UserRepo(users);
-
-    console.log('here: ', userRepo.users[0]);
-
     expect(userRepo.users[0].id).to.equal(1);
+    expect(userRepo.users[1].id).to.equal(2);
   });
 
   it('should return user data when given user ID', function() {
-
-
-    userRepo.getDataFromID(1);
-
     expect(userRepo.getDataFromID(1)).to.eql(user1);
+    expect(userRepo.getDataFromID(2)).to.eql(user2);
   });
 
   it('should return the average of all users step goals', function() {
