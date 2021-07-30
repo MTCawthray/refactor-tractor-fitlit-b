d45ec20fb229cf419aa1783e0b27f0ef3066712f
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-29 18:16:20 -0600

    Create sad path on hydration data test

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -107,6 +107,7 @@ describe('Hydration', function() {
     expect(hydration.hydrationData[0].userID).to.equal(1);
     expect(hydration.hydrationData[2].numOunces).to.equal(1);
     expect(hydration.hydrationData[4].date).to.equal('2018/10/23');
+    expect(hydration.hydrationData[1].id).to.equal(undefined);
   });
 
   it('should find water intake by day for first week', function() {
@@ -131,7 +132,6 @@ describe('Hydration', function() {
     });
     const users = [user3, user4];
     const userRepo = new UserRepo(users);
-    // console.log(hydration.calculateFirstWeekOunces(userRepo, 4));
     expect(hydration.calculateFirstWeekOunces(userRepo, 4)[0]).to.eql('2019/09/20: 40');
     expect(hydration.calculateFirstWeekOunces(userRepo, 4)[6]).to.eql('2019/04/15: 36');
   });
@@ -158,7 +158,6 @@ describe('Hydration', function() {
     });
     const users = [user3, user4];
     const userRepo = new UserRepo(users);
-    console.log("HELOOO", hydration.calculateRandomWeekOunces('2018/02/01', 4, userRepo));
     expect(hydration.calculateRandomWeekOunces('2019/09/18', 4, userRepo)[0]).to.eql('2019/09/18: 40');
     // expect(hydration.calculateRandomWeekOunces('2018/02/01', 4, userRepo)[6]).to.eql('2019/09/16: 30');
     //this is failing because it doesn't exist, need a failure case
