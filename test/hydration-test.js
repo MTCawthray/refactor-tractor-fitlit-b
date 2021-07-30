edd0a740f7d2543a5dacc784c8d6ef99c3253ca1
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-29 18:36:56 -0600

    Add reassingments to before each and add sad path to claculate random week ounces

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -4,10 +4,30 @@ import UserRepo from '../src/User-repo';
 import User from '../src/User';
 
 describe('Hydration', function() {
-  let hydrationData;
-  let hydration;
+  let hydration, hydrationData, user3, user4, users, userRepo;
+  
 
   beforeEach(function() {
+    user3 = new User({
+      id: 3,
+      name: "The Rock",
+      address: "1236 Awesome Street, Denver CO 80301-1697",
+      email: "therock@hotmail.com",
+      strideLength: 10,
+      dailyStepGoal: 60000,
+      friends: [1, 2, 4]
+    });
+    user4 = new User({
+      id: 4,
+      name: "Rainbow Dash",
+      address: "1237 Equestria Street, Denver CO 80301-1697",
+      email: "rainbowD1@hotmail.com",
+      strideLength: 3.8,
+      dailyStepGoal: 7000,
+      friends: [1, 2, 3]
+    });
+    users = [user3, user4];
+    userRepo = new UserRepo(users);
     hydrationData = [{
       "userID": 1,
       "date": "2019/06/15",
@@ -98,8 +118,7 @@ describe('Hydration', function() {
       "date": "2019/09/15",
       "numOunces": 30
     },
-    ]
-
+    ];
     hydration = new Hydration(hydrationData);
   });
 
@@ -111,56 +130,14 @@ describe('Hydration', function() {
   });
 
   it('should find water intake by day for first week', function() {
-    const user3 = new User({
-      id: 3,
-      name: "The Rock",
-      address: "1236 Awesome Street, Denver CO 80301-1697",
-      email: "therock@hotmail.com",
-      strideLength: 10,
-      dailyStepGoal: 60000,
-      friends: [1, 2, 4]
-    });
-
-    const user4 = new User({
-      id: 4,
-      name: "Rainbow Dash",
-      address: "1237 Equestria Street, Denver CO 80301-1697",
-      email: "rainbowD1@hotmail.com",
-      strideLength: 3.8,
-      dailyStepGoal: 7000,
-      friends: [1, 2, 3]
-    });
-    const users = [user3, user4];
-    const userRepo = new UserRepo(users);
     expect(hydration.calculateFirstWeekOunces(userRepo, 4)[0]).to.eql('2019/09/20: 40');
     expect(hydration.calculateFirstWeekOunces(userRepo, 4)[6]).to.eql('2019/04/15: 36');
   });
 
-  it('should find sleep quality by day for that days week', function() {
-    const user3 = new User({
-      id: 3,
-      name: "The Rock",
-      address: "1236 Awesome Street, Denver CO 80301-1697",
-      email: "therock@hotmail.com",
-      strideLength: 10,
-      dailyStepGoal: 60000,
-      friends: [1, 2, 4]
-    });
-
-    const user4 = new User({
-      id: 4,
-      name: "Rainbow Dash",
-      address: "1237 Equestria Street, Denver CO 80301-1697",
-      email: "rainbowD1@hotmail.com",
-      strideLength: 3.8,
-      dailyStepGoal: 7000,
-      friends: [1, 2, 3]
-    });
-    const users = [user3, user4];
-    const userRepo = new UserRepo(users);
+  it('should find ounces drank for that days week', function() {
+    console.log(hydration.calculateRandomWeekOunces('2018/02/01', 4, userRepo)[6]);
     expect(hydration.calculateRandomWeekOunces('2019/09/18', 4, userRepo)[0]).to.eql('2019/09/18: 40');
-    // expect(hydration.calculateRandomWeekOunces('2018/02/01', 4, userRepo)[6]).to.eql('2019/09/16: 30');
-    //this is failing because it doesn't exist, need a failure case
+    expect(hydration.calculateRandomWeekOunces('2018/02/01', 4, userRepo)[6]).to.eql(undefined);
   })
   //day of hydration should not include user 2 or user 1 on August 22
   //week of hydration should not include user 4 not during the week
