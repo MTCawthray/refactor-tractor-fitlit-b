a9a8ae34ea7286aa7e6cc389c33427ff05ff6fd7
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-30 15:41:57 -0600

    Remove uredundant before each and refactor get friends avg steps week method

--- test/activity-test.js
+++ test/activity-test.js
@@ -65,232 +65,160 @@ describe('Activity', function() {
     expect(activity.activityData[10].flightsOfStairs).to.eql(5);
   });
 
-
-
 describe('Friend Activity', function() {
-  let activityData, activity, user1, user2, user3, user4, user5, users, userRepo;
-
-  beforeEach(function() {
-    activityData = [{
-      "userID": 1,
-      "date": "2019/06/15",
-      "numSteps": 3577,
-      "minutesActive": 140,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 2,
-      "date": "2019/06/14",
-      "numSteps": 4294,
-      "minutesActive": 138,
-      "flightsOfStairs": 10
-    },
-    {
-      "userID": 3,
-      "date": "2019/06/13",
-      "numSteps": 7402,
-      "minutesActive": 116,
-      "flightsOfStairs": 33
-    },
-    {
-      "userID": 4,
-      "date": "2019/06/12",
-      "numSteps": 3486,
-      "minutesActive": 114,
-      "flightsOfStairs": 32
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/14",
-      "numSteps": 11374,
-      "minutesActive": 213,
-      "flightsOfStairs": 13
-    },
-    {
-      "userID": 2,
-      "date": "2019/06/13",
-      "numSteps": 14810,
-      "minutesActive": 287,
-      "flightsOfStairs": 18
-    },
-    {
-      "userID": 3,
-      "date": "2019/06/12",
-      "numSteps": 2634,
-      "minutesActive": 107,
-      "flightsOfStairs": 5
-    },
-    {
-      "userID": 4,
-      "date": "2019/06/11",
-      "numSteps": 10333,
-      "minutesActive": 114,
-      "flightsOfStairs": 31
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/02",
-      "numSteps": 6389,
-      "minutesActive": 41,
-      "flightsOfStairs": 33
-    },
-    {
-      "userID": 2,
-      "date": "2019/06/03",
-      "numSteps": 8015,
-      "minutesActive": 106,
-      "flightsOfStairs": 37
-    },
-    {
-      "userID": 3,
-      "date": "2019/06/19",
-      "numSteps": 11652,
-      "minutesActive": 20,
-      "flightsOfStairs": 24
-    },
-    {
-      "userID": 4,
-      "date": "2019/06/15",
-      "numSteps": 9256,
-      "minutesActive": 108,
-      "flightsOfStairs": 2
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/16",
-      "numSteps": 3578,
-      "minutesActive": 140,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/17",
-      "numSteps": 3579,
-      "minutesActive": 141,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/18",
-      "numSteps": 3580,
-      "minutesActive": 142,
-      "flightsOfStairs": 16
-    }
-    ];
-    activity = new Activity(activityData);
-    user1 = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    user2 = new User({
-      id: 2,
-      name: "Allie McCarthy",
-      address: "1235 Turing Street, Denver CO 80301-1697",
-      email: "allie.mcc1@hotmail.com",
-      strideLength: 3.3,
-      dailyStepGoal: 9000,
-      friends: [1, 3, 4]
-    });
-    user3 = new User({
-      id: 3,
-      name: "The Rock",
-      address: "1236 Awesome Street, Denver CO 80301-1697",
-      email: "therock@hotmail.com",
-      strideLength: 10,
-      dailyStepGoal: 60000,
-      friends: [1, 2, 4]
-    });
-    user4 = new User({
-      id: 4,
-      name: "Rainbow Dash",
-      address: "1237 Equestria Street, Denver CO 80301-1697",
-      email: "rainbowD1@hotmail.com",
-      strideLength: 3.8,
-      dailyStepGoal: 7000,
-      friends: [1, 2]
-    });
-    users = [user1, user2, user3, user4];
-    userRepo = new UserRepo(users);
-  });
-
+ 
   it('should get a users friend lists activity', function() {
-    expect(activity.getFriendsActivity(user4, userRepo)).to.eql([{
-      "userID": 1,
-      "date": "2019/06/15",
-      "numSteps": 3577,
-      "minutesActive": 140,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/14",
-      "numSteps": 11374,
-      "minutesActive": 213,
-      "flightsOfStairs": 13
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/02",
-      "numSteps": 6389,
-      "minutesActive": 41,
-      "flightsOfStairs": 33
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/16",
-      "numSteps": 3578,
-      "minutesActive": 140,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/17",
-      "numSteps": 3579,
-      "minutesActive": 141,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 1,
-      "date": "2019/06/18",
-      "numSteps": 3580,
-      "minutesActive": 142,
-      "flightsOfStairs": 16
-    },
-    {
-      "userID": 2,
-      "date": "2019/06/14",
-      "numSteps": 4294,
-      "minutesActive": 138,
-      "flightsOfStairs": 10
-    },
-    {
-      "userID": 2,
-      "date": "2019/06/13",
-      "numSteps": 14810,
-      "minutesActive": 287,
-      "flightsOfStairs": 18
-    },
-    {
-      "userID": 2,
-      "date": "2019/06/03",
-      "numSteps": 8015,
-      "minutesActive": 106,
-      "flightsOfStairs": 37
-    }
+    // console.log(activity.getFriendsActivity(user4, userRepo));
+    expect(activity.getFriendsActivity(user4, userRepo)).to.eql([
+      {
+        userID: 1,
+        date: '2019/06/15',
+        numSteps: 3577,
+        minutesActive: 140,
+        flightsOfStairs: 16
+      },
+      {
+        userID: 1,
+        date: '2019/06/16',
+        numSteps: 6637,
+        minutesActive: 175,
+        flightsOfStairs: 36
+      },
+      {
+        userID: 1,
+        date: '2019/06/17',
+        numSteps: 14329,
+        minutesActive: 168,
+        flightsOfStairs: 18
+      },
+      {
+        userID: 1,
+        date: '2019/06/18',
+        numSteps: 4419,
+        minutesActive: 165,
+        flightsOfStairs: 33
+      },
+      {
+        userID: 1,
+        date: '2019/06/19',
+        numSteps: 8429,
+        minutesActive: 275,
+        flightsOfStairs: 2
+      },
+      {
+        userID: 1,
+        date: '2019/06/20',
+        numSteps: 14478,
+        minutesActive: 140,
+        flightsOfStairs: 12
+      },
+      {
+        userID: 1,
+        date: '2019/06/21',
+        numSteps: 6760,
+        minutesActive: 135,
+        flightsOfStairs: 6
+      },
+      {
+        userID: 1,
+        date: '2019/06/22',
+        numSteps: 10289,
+        minutesActive: 119,
+        flightsOfStairs: 6
+      },
+      {
+        userID: 1,
+        date: '2019/06/23',
+        numSteps: 13928,
+        minutesActive: 218,
+        flightsOfStairs: 21
+      },
+      {
+        userID: 1,
+        date: '2019/06/24',
+        numSteps: 7186,
+        minutesActive: 25,
+        flightsOfStairs: 15
+      },
+      {
+        userID: 2,
+        date: '2019/06/15',
+        numSteps: 4294,
+        minutesActive: 138,
+        flightsOfStairs: 10
+      },
+      {
+        userID: 2,
+        date: '2019/06/16',
+        numSteps: 4112,
+        minutesActive: 220,
+        flightsOfStairs: 37
+      },
+      {
+        userID: 2,
+        date: '2019/06/17',
+        numSteps: 13750,
+        minutesActive: 65,
+        flightsOfStairs: 4
+      },
+      {
+        userID: 2,
+        date: '2019/06/18',
+        numSteps: 4662,
+        minutesActive: 181,
+        flightsOfStairs: 31
+      },
+      {
+        userID: 2,
+        date: '2019/06/19',
+        numSteps: 9858,
+        minutesActive: 243,
+        flightsOfStairs: 44
+      },
+      {
+        userID: 2,
+        date: '2019/06/20',
+        numSteps: 8153,
+        minutesActive: 74,
+        flightsOfStairs: 10
+      },
+      {
+        userID: 2,
+        date: '2019/06/21',
+        numSteps: 10225,
+        minutesActive: 174,
+        flightsOfStairs: 26
+      },
+      {
+        userID: 2,
+        date: '2019/06/22',
+        numSteps: 3605,
+        minutesActive: 124,
+        flightsOfStairs: 31
+      },
+      {
+        userID: 2,
+        date: '2019/06/23',
+        numSteps: 4148,
+        minutesActive: 142,
+        flightsOfStairs: 0
+      },
+      {
+        userID: 2,
+        date: '2019/06/24',
+        numSteps: 8568,
+        minutesActive: 81,
+        flightsOfStairs: 31
+      }
     ]);
   });
 
   it('should get a users ranked friendslist activity for a chosen week', function() {
     expect(activity.getFriendsAverageStepsForWeek(user4, "2019/06/15", userRepo)).to.eql([{
-      '2': 9552
+      '2': 4294
     },
     {
-      '1': 7475.5
+      '1': 3577
     }
     ]);
   });
