c7a5f34932327c71a63214dbeb603d99964f65c0
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-30 18:20:08 -0600

    Convert all functions to fat arrow in activity test file

--- test/activity-test.js
+++ test/activity-test.js
@@ -4,10 +4,10 @@ import UserRepo from '../src/User-repo';
 import User from '../src/User';
 import sampleActivityData from '../src/data/sample-activity.js'
 
-describe('Activity', function() {
+describe('Activity', () => {
   let activityData, user1, user2, user3, user4, users, userRepo, activity;
 
-  beforeEach(function() {
+  beforeEach(() => {
     activityData = sampleActivityData;
     user1 = new User({
       id: 1,
@@ -57,7 +57,7 @@ describe('Activity', function() {
     expect(activity).to.be.an.instanceOf(Activity);
   });
 
-  it('should take in data', function() {
+  it('should take in data', () => {
     expect(activity.activityData[0].userID).to.eql(1);
     expect(activity.activityData[4].date).to.eql("2019/06/16");
     expect(activity.activityData[3].numSteps).to.eql(3486);
@@ -65,14 +65,13 @@ describe('Activity', function() {
     expect(activity.activityData[10].flightsOfStairs).to.eql(5);
   });
 
-describe('getFriendActivity()', function() {
+describe('getFriendActivity()', () => {
 
   it('should return an array', () => {
     expect(activity.getFriendsActivity(user3, userRepo)).to.be.an('array');
   });
  
-  it('should get a users friend lists activity', function() {
-    // console.log(activity.getFriendsActivity(user4, userRepo));
+  it('should get a users friend lists activity', () => {
     expect(activity.getFriendsActivity(user4, userRepo)).to.eql([
       {
         userID: 1,
@@ -218,12 +217,12 @@ describe('getFriendActivity()', function() {
   });
 });
 
-describe('getFriendsAverageStepsForWeek()', function() {
+describe('getFriendsAverageStepsForWeek()', () => {
   it('should return an array', () => {
     expect(activity.getFriendsAverageStepsForWeek(user2, "2019/06/23", userRepo)).to.be.an('array');
   });
 
-  it('should get a users ranked friendslist activity for a chosen week', function() {
+  it('should get a users ranked friendslist activity for a chosen week', () => {
     expect(activity.getFriendsAverageStepsForWeek(user4, "2019/06/15", userRepo)).to.eql([{'2': 4294}, {'1': 3577}]);
 
     expect(activity.getFriendsAverageStepsForWeek(user4, "2019/06/23", userRepo)).to.eql([{'1': 9908.625}, {'2': 7314.125}]);
@@ -231,35 +230,35 @@ describe('getFriendsAverageStepsForWeek()', function() {
 
 });
 
-describe('showChallengeListAndWinner()', function() {
+describe('showChallengeListAndWinner()', () => {
   it('should be an array', () => {
     expect(activity.showChallengeListAndWinner(user2, "2019/06/23", userRepo)).to.be.an('array');
   });
 
-  it('should get a users ranked friendslist activity for a chosen week with names', function() {
+  it('should get a users ranked friendslist activity for a chosen week with names', () => {
     expect(activity.showChallengeListAndWinner(user4, "2019/06/15", userRepo)).to.eql([
       'Allie McCarthy: 4294', 'Alex Roth: 3577'
     ])
   });
 });
 
-describe('getWinnerId', function() {
+describe('getWinnerId', () => {
   it('should return a number', () => {
     expect(activity.getWinnerId(user2, '2019/06/23', userRepo)).to.be.a('number')
   });
 
-  it('should know the ID of the winning friend', function() {
+  it('should know the ID of the winning friend', () => {
     expect(activity.getWinnerId(user2, '2019/06/23', userRepo)).to.eql(1)
     expect(activity.getWinnerId(user4, "2019/06/15", userRepo)).to.eql(2)
   });
 });
 
-describe('getStreak()', function() {
-  it('should show a 3-day increasing streak for a users step count', function() {
+describe('getStreak()', () => {
+  it('should show a 3-day increasing streak for a users step count', () => {
     expect(activity.getStreak(userRepo, 1, 'numSteps')).to.eql(['2019/06/17', '2019/06/20', '2019/06/23'])
   });
 
-  it('should show a 3-day increasing streak for a users minutes of activity', function() {
+  it('should show a 3-day increasing streak for a users minutes of activity', () => {
     expect(activity.getStreak(userRepo, 2, 'minutesActive')).to.eql(['2019/06/19'])
   });
 });
