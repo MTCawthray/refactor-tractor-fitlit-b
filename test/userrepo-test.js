98cf4ffd0d66ed9fb92ec72a96b976e62b90ff44
darlaevans2000 <darlaevans2000@gmail.com>
2021-08-02 17:36:28 -0600

    Fix userrepo test linter warnings

--- test/userrepo-test.js
+++ test/userrepo-test.js
@@ -82,75 +82,75 @@ describe('User Repo', () => {
     it('should get a users data from its userID in any data set', () => {
 
       expect(userRepo.getDataFromUserID(1, hydrationData)).to.eql([{
-          userID: 1,
-          date: '2019/06/15',
-          numOunces: 37
-        },
-        {
-          userID: 1,
-          date: '2019/06/16',
-          numOunces: 69
-        },
-        {
-          userID: 1,
-          date: '2019/06/17',
-          numOunces: 96
-        },
-        {
-          userID: 1,
-          date: '2019/06/18',
-          numOunces: 61
-        },
-        {
-          userID: 1,
-          date: '2019/06/19',
-          numOunces: 91
-        },
-        {
-          userID: 1,
-          date: '2019/06/20',
-          numOunces: 50
-        },
-        {
-          userID: 1,
-          date: '2019/06/21',
-          numOunces: 50
-        },
-        {
-          userID: 1,
-          date: '2019/06/22',
-          numOunces: 43
-        },
-        {
-          userID: 1,
-          date: '2019/06/23',
-          numOunces: 39
-        },
-        {
-          userID: 1,
-          date: '2019/06/24',
-          numOunces: 61
-        },
-        {
-          userID: 1,
-          date: '2019/06/25',
-          numOunces: 51
-        },
-        {
-          userID: 1,
-          date: '2019/06/26',
-          numOunces: 52
-        },
-        {
-          userID: 1,
-          date: '2019/06/27',
-          numOunces: 29
-        },
-        {
-          userID: 1,
-          date: '2019/06/28',
-          numOunces: 57
-        }
+        userID: 1,
+        date: '2019/06/15',
+        numOunces: 37
+      },
+      {
+        userID: 1,
+        date: '2019/06/16',
+        numOunces: 69
+      },
+      {
+        userID: 1,
+        date: '2019/06/17',
+        numOunces: 96
+      },
+      {
+        userID: 1,
+        date: '2019/06/18',
+        numOunces: 61
+      },
+      {
+        userID: 1,
+        date: '2019/06/19',
+        numOunces: 91
+      },
+      {
+        userID: 1,
+        date: '2019/06/20',
+        numOunces: 50
+      },
+      {
+        userID: 1,
+        date: '2019/06/21',
+        numOunces: 50
+      },
+      {
+        userID: 1,
+        date: '2019/06/22',
+        numOunces: 43
+      },
+      {
+        userID: 1,
+        date: '2019/06/23',
+        numOunces: 39
+      },
+      {
+        userID: 1,
+        date: '2019/06/24',
+        numOunces: 61
+      },
+      {
+        userID: 1,
+        date: '2019/06/25',
+        numOunces: 51
+      },
+      {
+        userID: 1,
+        date: '2019/06/26',
+        numOunces: 52
+      },
+      {
+        userID: 1,
+        date: '2019/06/27',
+        numOunces: 29
+      },
+      {
+        userID: 1,
+        date: '2019/06/28',
+        numOunces: 57
+      }
       ]);
     });
 
